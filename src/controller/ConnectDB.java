1c80b2ac1367eb36a64c3e858b1f25f37418cf98
VU THI THU HANG 20183524 <hang.vtt183524@sis.hust.edu.vn>
2020-11-08 13:08:53 +0700

    create class ConnectDB

---
+++ src/controller/ConnectDB.java
@@ -0,0 +1,18 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ConnectDB {
+	private Connection conn;
+	private Statement stat;
+	private ResultSet rs;
+	
+	
+	public static void main(String args[])
+	{
+		System.out.print("done");
+	}
+	
+}
