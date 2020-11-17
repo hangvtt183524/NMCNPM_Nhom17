f2aaaed16f694b668d8f681789f2220799bc23e4
thule <baekhyunee129@gmail.com>
2020-11-17 18:01:45 +0700

    update class FormFunction and Human

--- src/gui/FormFunction.java
+++ src/gui/FormFunction.java
@@ -1,5 +1,33 @@
 package gui;
 
-public class FormFunction {
+import java.sql.ResultSet;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+
+public class FormFunction extends GridPane {
+	protected TextArea searchText;
+	protected Button saveBtn;
+	protected Button searchBtn;
+	protected Button addBtn;
+	protected Button changeBtn;
+	protected TableView<Human> ResultSet;
+	
+	public void save_event(String query) {
+		
+	}
+	
+	public void add_event() {
+		
+	}
+	
+	public void change_event(String query) {
+		
+	}
+	
+	public ResultSet search_event(String query) {
+		return null;
+	}
 
 }
